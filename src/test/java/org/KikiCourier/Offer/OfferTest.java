d9bcf19e50dd67d01750f3c4fb46ee63a32c08ae
Manish <Manishy-97>
2024-09-02 16:04:07 +0530

    [Manish]: refactor offer test

--- src/test/java/org/KikiCourier/Offer/OfferTest.java
+++ src/test/java/org/KikiCourier/Offer/OfferTest.java
@@ -1,47 +1,48 @@
 package org.KikiCourier.Offer;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OfferTest {
 
+    private Offer offer;
+
+    @BeforeEach
+    public void setUp() {
+        OfferRule offerRule = new OfferRule(70, 200, 0, 200);
+        offer = new Offer("OFR01", 4, offerRule);
+    }
+
     @Test
     public void calculateDiscount_shouldCalculateDiscount() {
-        Offer ofr01 = new Offer("OFR01", 4, null);
-        Double discount = ofr01.calculateDiscount(200.00);
+        Double discount = offer.calculateDiscount(200.00);
         assertEquals(8, discount);
     }
 
     @Test
-    public void isValid_shouldReturnTrueIfOfferCodeIsNotNullAndValid() {
-        Offer ofr01 = new Offer("OFR01", 4, null);
-        assertTrue(ofr01.isValid("OFR01"));
+    public void isValid_shouldReturnTrueIfOfferCodeIsValid() {
+        assertTrue(offer.isValid("OFR01"));
     }
 
     @Test
-    public void isValid_shouldReturnFalseIfOfferCodeIsNotNullAndInValid() {
-        Offer ofr01 = new Offer("OFR01", 4, null);
-        assertFalse(ofr01.isValid("OFR02"));
+    public void isValid_shouldReturnFalseIfOfferCodeIsInValid() {
+        assertFalse(offer.isValid("OFR02"));
     }
 
     @Test
     public void isValid_shouldReturnFalseIfOfferCodeIsNull() {
-        Offer ofr01 = new Offer(null, 4, null);
-        assertFalse(ofr01.isValid("OFR02"));
+        assertFalse(offer.isValid(null));
     }
 
     @Test
     public void isApplicable_shouldReturnTrueIfOfferIsNotApplicable() {
-        OfferRule offerRule = new OfferRule(70, 200, 0, 200);
-        Offer ofr01 = new Offer("OFR01", 4, offerRule);
-        assertTrue(ofr01.isApplicable(80, 10));
+        assertTrue(offer.isApplicable(80, 10));
     }
 
     @Test
     public void isApplicable_shouldReturnTrueIfOfferIsApplicable() {
-        OfferRule offerRule = new OfferRule(70, 200, 0, 200);
-        Offer ofr01 = new Offer("OFR01", 4, offerRule);
-        assertFalse(ofr01.isApplicable(60, 10));
+        assertFalse(offer.isApplicable(60, 10));
     }
 }
